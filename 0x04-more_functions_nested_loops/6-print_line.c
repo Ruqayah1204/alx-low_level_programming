143631b0a38374d46ed3c768f60d692b1feeb0a0
Ruqayah1204 <mhiedey98@gmail.com>
2023-06-22 09:06:43 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - short description, single line
+ * @n: Argument
+(* a blank line
+ * description: longer description of the function)?
+(* section header: section description)*
+ * return: void
+*/
+void print_line(int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+	if (n > 0)
+	{
+		_putchar('_');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+	}
+	_putchar('\n');
+}
