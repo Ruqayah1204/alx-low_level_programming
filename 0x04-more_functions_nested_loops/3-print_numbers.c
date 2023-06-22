445fab7e8d6b6c8fe3bcbf9b7176b7c7f6f2a59c
Ruqayah1204 <mhiedey98@gmail.com>
2023-06-22 08:35:23 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,11 @@
  */
 void print_numbers(void)
 {
-int i = 0;
-while (i <= 9)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
